60f61ad4800ef0c24c242f7e3c7fa508cacae0df
rkinger85@gmail.com <freeware1>
2016-11-16 22:22:14 +0800

    changes for runnable jar included

---
+++ src/main/java/com/gnb/springboot/FirstApp.java
@@ -0,0 +1,24 @@
+package com.gnb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by shiva on 16/11/16.
+ */
+@RestController
+@EnableAutoConfiguration
+public class FirstApp {
+
+  @RequestMapping("/")
+public String home(){
+    return "Hello World !";
+}
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApp.class,args);
+    }
+
+}
